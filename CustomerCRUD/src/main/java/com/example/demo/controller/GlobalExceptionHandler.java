5a6d8a3fc47719ad732e9fe28b3830ee47cbfb65
Gargiii20 <gargi.kulkarni.work@gmail.com>
2026-02-01 12:27:45 +0530

    Hii

--- CustomerCRUD/src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -13,13 +13,14 @@ import com.example.demo.exception.InvalidMobNumber;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(InvalidMobNumber.class)
-	public ResponseEntity<?> invalidMobNumber(InvalidMobNumber e){
+	public ResponseEntity<?> invalidMobNumber(InvalidMobNumber e) {
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(e.getMessage());
-		
+
 	}
+
 	@ExceptionHandler(InvalidId.class)
-	public ResponseEntity<?> invalidId(InvalidId i){
+	public ResponseEntity<?> invalidId(InvalidId i) {
 		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(i.getMessage());
-		
+
 	}
 }
